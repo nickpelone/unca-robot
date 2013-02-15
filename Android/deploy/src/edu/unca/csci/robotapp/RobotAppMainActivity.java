374eed11d916ff0d3ebadb0799f34c81b8753a68
Nick Pelone <npelone@unca.edu>
2013-02-15 11:56:49 -0500

    Scrolling debugTextView and button that publishes output - the basics of a debug console

--- Android/deploy/src/edu/unca/csci/robotapp/RobotAppMainActivity.java
+++ Android/deploy/src/edu/unca/csci/robotapp/RobotAppMainActivity.java
@@ -2,17 +2,35 @@ package edu.unca.csci.robotapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class RobotAppMainActivity extends Activity {
+	private Button theButton;
+	private TextView theText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		TextView debugMessageArea = new TextView(this);
 		super.onCreate(savedInstanceState);
-		Button button = new Button(this);
 		setContentView(R.layout.activity_robot_app_main);
+		theButton = (Button) findViewById(R.id.exampleButton);
+		theText = (TextView) findViewById(R.id.exampleText);
+		 final String returnable = "hello";
+		 theText.setMovementMethod(new ScrollingMovementMethod());
+		
+		theButton.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				theText.append('\n' + returnable);
+				
+				
+			}
+		});
 	}
 
 	@Override
