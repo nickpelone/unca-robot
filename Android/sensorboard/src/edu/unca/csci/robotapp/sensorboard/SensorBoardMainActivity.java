f641a9aca94a8245f033b5e14e274d6983792fe9
Nick Pelone <npelone@unca.edu>
2013-03-06 15:24:05 -0500

    Cleans up layout, adds reset button to clear text field with results, other little cleanups

--- Android/sensorboard/src/edu/unca/csci/robotapp/sensorboard/SensorBoardMainActivity.java
+++ Android/sensorboard/src/edu/unca/csci/robotapp/sensorboard/SensorBoardMainActivity.java
@@ -8,13 +8,14 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ToggleButton;
 import edu.unca.csci.foundation.DebugTextView;
 
 public class SensorBoardMainActivity extends Activity implements SensorEventListener {
-	
+	public final String reset = "Reset";
 	protected void onCreate(Bundle savedInstanceState) {
 		
 	
@@ -47,9 +48,18 @@ public class SensorBoardMainActivity extends Activity implements SensorEventList
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.sensor_board_main, menu);
+			
+		menu.add(reset);
 		return true;
 	}
+	public boolean onOptionsItemSelected (MenuItem mi){
+		if(mi.getTitle() == reset){
+			DebugTextView dbg = (DebugTextView) findViewById(R.id.debugTextView1);
+			dbg.setText("");
 
+		}
+		return true;
+	}
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
 		// TODO Auto-generated method stub
@@ -65,7 +75,7 @@ public class SensorBoardMainActivity extends Activity implements SensorEventList
 		accelX = event.values[0];
 		accelY = event.values[1];
 		accelZ = event.values[2];
-		dbg.debugAppend(""+accelX+'\n'+accelY+'\n'+accelZ);
+		dbg.debugAppend("!!NEW ACCELEROMETER VALUES!!"+'\n'+"X:"+accelX+'\n'+"Y:"+accelY+'\n'+"Z:"+accelZ);
 		
 		
 	}
