8b9e43326183ad48907dee00b444010e0b8f14a7
Nick Pelone <npelone@unca.edu>
2013-03-08 11:28:58 -0500

    More setup for CompassActivity

--- Android/deploy/src/edu/unca/csci/robotapp/CompassActivity.java
+++ Android/deploy/src/edu/unca/csci/robotapp/CompassActivity.java
@@ -4,20 +4,26 @@ import edu.unca.csci.foundation.DebugTextView;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 import android.widget.ScrollView;
 
 public class CompassActivity extends Activity implements SensorEventListener {
-	private final SensorEventListener sel = this;
-	private final DebugTextView dbg = (DebugTextView) findViewById(R.id.compassDebugTextView1);
-	private final ScrollView scroller = (ScrollView) findViewById(R.id.compassScrollView1);
-	
+ 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_compass);
+		//
+		final SensorEventListener sel = this;
+		final ScrollView scroller = (ScrollView) findViewById(R.id.compassScrollView1);
+		final SensorManager compassSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		final Sensor compass = compassSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		final DebugTextView dbg = (DebugTextView) findViewById(R.id.compassDebugTextView1);
+		dbg.setText("No compass data yet");
 	}
 
 	@Override
@@ -35,8 +41,16 @@ public class CompassActivity extends Activity implements SensorEventListener {
 
 	@Override
 	public void onSensorChanged(SensorEvent arg0) {
+		DebugTextView dbg1 = (DebugTextView) findViewById(R.id.compassDebugTextView1);
 		// TODO Auto-generated method stub
 		//when the sensor gets new values different from old ones this code is executed
+		float x,y,z;
+		x = arg0.values[0];
+		y = arg0.values[1];
+		z = arg0.values[2];
+		
+		dbg1.debugAppend("magnetic X: "+x+'\n'+"magnetic Y: "+y+'\n'+"magnetic Z: "+z);
+		
 		
 	}
 
