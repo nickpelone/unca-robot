7421e1314815f1c183065e17ffdaf23d267008c3
Nick Pelone <npelone@unca.edu>
2013-03-24 13:31:24 -0400

    Bluetooth bootload test code

---
+++ Arduino/bluetooth_softserial_bootload/bluetooth_softserial_bootload.cpp
@@ -0,0 +1,13 @@
+#include <WProgram.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetoothSerial(5,6);
+
+void setup(){
+  bluetoothSerial.begin(115200);
+}
+
+void loop(){
+  bluetoothSerial.println("Hey");
+}
+
