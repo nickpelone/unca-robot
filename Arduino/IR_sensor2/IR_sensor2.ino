8027d76e3e70b6ff432056e93bd75a4317c7518b
Nick Pelone <npelone@unca.edu>
2013-03-30 18:17:43 -0400

    Adds basic functionality for IR sensor checking. Should be drop-in-able to the main robot application

--- Arduino/IR_sensor2/IR_sensor2.ino
+++ Arduino/IR_sensor2/IR_sensor2.ino
@@ -1,5 +1,6 @@
 int irPin1 = 50;  //Front IR pin
 int irPin2 = 51;  //Rear IR pin
+boolean irStatus = false; //Are we in position via IR? true means stop
 
 void setup() {
   Serial.begin(9600);
@@ -24,20 +25,37 @@ void checkIRs() {
   }
   else if (digitalRead(irPin2) != 1) {
     slightBackup();
-  }
-  else Serial.println("no move needed.");  
+  }else{
+  michaelRobot.debugPrintLn("no move needed.");
+  irstatus = true;
   delay(1000);
 }
 
 void slightBackup()  {
  //backup robot slightly 
- Serial.println("Backing up slightly");
+ michaelRobot.debugPrintLn("Backing up slightly");
+ while(irStatus == false){
+   //we are not in position
+   
+   //move forward super slow
+   michaelRobot.setSpeed(0,false, int(60*0.96));
+   michaelRobot.setSpeed(0,false,60)
+   
+   delay(1500);
+   checkIRS();
+   
   
+  }
 }
 
 void slightForward()  {
   //go forward slightly
-  Serial.println("Moving Forward slightly");
-  
+  michaelRobot.debugPrintLn("Moving Forward slightly");
+    while(irStatus == false){
+      michaelRobot.setSpeed(0,true, int(60*0.96));
+      michaelRobot.setSpeed(1,true,60);
+      delay(1500);
+      checkIRS();
+    }
 }
 
